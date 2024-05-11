7ddc8c050e5b0e5b0f87b54f07d24ae1af468dd3
Rishabh kumar <83425253+Rishabhk0207@users.noreply.github.com>
2024-05-11 13:29:21 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,13 @@
 function firstChar(text) {
   // your code here
+	let ans="";
+	text =text.trim();
+	ans += text.charAt(0);
+	return ans;
 }
 
 // Do not change the code below
 
 const text = prompt("Enter text:");
 alert(firstChar(text));
+      
